153b4364fddd3fcb2e1049c837328bfdf126f77c
topu9872-cpu <topu9872@gmail.com>
2026-03-23 22:54:10 +0600

    commit2

--- src/componets/navbar/Link.jsx
+++ src/componets/navbar/Link.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Link = ({route}) => {
     return (
        
-      <il className='mr-10'> 
-  <a href={route.path}>{route.name}</a>
+      <il className='lg:mr-10 px-4  hover:bg-amber-400'> 
+  <a  href={route.path}>{route.name}</a>
       </il>
       
       
--- src/componets/navbar/NavBar.jsx
+++ src/componets/navbar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from 'react';
+ import Link from './Link';
+import { Menu, X } from 'lucide-react';
 const navigationData = [
   { "id": 1, "name": "Home", "path": "/" },
   { "id": 2, "name": "About", "path": "/about" },
@@ -8,13 +9,28 @@ const navigationData = [
 ];
 
 const NavBar = () => {
+
+const [open, setOpen] = useState(false);
+
+const links =  navigationData.map(route=><Link key={route.id} route={route}></Link>);
+
     return (
-        <nav>
+        <nav className='flex justify-between mx-10 my-4'>
+          
+<span className='flex gap-10' onClick={()=>setOpen(!open)}>
+    {open?
+     <X className='md:hidden'></X>
+     :<Menu className=' md:hidden'></Menu>}
+  <ul className={`grid md:hidden text-black absolute duration-1000 ${open ? 'top-10' : '-top-40'} bg-amber-200`}>
+      {links}
+  </ul>
 
+    <h3> My Navbar</h3>
+</span>
 
-    <ul className='flex'>
+    <ul className='md:flex hidden'>
         {
-        navigationData.map(route=><Link key={route.id} route={route}></Link>)
+        links
 }
     </ul>
 
@@ -30,6 +46,7 @@ const NavBar = () => {
         {/* <li><a href="/about">About</a></li> */}
         {/* <li><a href="/blog">Blog</a></li> */}
     {/* </ul> */}
+    <button className='btn btn-dash btn-info'>Sing In</button>
        </nav>
     );
 };
